0dddae0b6e019882c2965cdeb5ec31d6e6df358c
kartsuri89 <kartsuri89@gmail.com>
2017-04-09 17:17:11 +0530

    Check 1

---
+++ src/main/java/BootVertx/BootVertxApplication.java
@@ -0,0 +1,12 @@
+package BootVertx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVertxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootVertxApplication.class, args);
+	}
+}
